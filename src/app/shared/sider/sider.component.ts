0721703e385330ddfc87c4b6c882a2bafc28e528
jseguraj1991 <jseguraj1991@gmail.com>
2020-08-19 13:21:24 +0200

    first commit

---
+++ src/app/shared/sider/sider.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sider',
+  templateUrl: './sider.component.html',
+  styles: [
+  ]
+})
+export class SiderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
